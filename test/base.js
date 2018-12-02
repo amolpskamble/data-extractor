6021c7930c07235b8e8e640ef38898c1643c700d
Amol Kamble <amol.p.s.kamble@gmail.com>
2018-12-03 00:46:28 +0530

    base tests

---
+++ test/base.js
@@ -0,0 +1,15 @@
+const chai = require('chai');
+let should = chai.should();
+
+const lib = require('../lib')
+
+describe('Data extractor base function testing', function() {
+    it('It should return library function', function(done) {
+        lib.should.be.a('function')
+        done()
+    });
+    it('It should return return true', function(done) {
+        lib().should.be.eq(true)
+        done()
+    });
+});
